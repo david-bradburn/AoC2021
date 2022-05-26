bedde30fe074430fe026e695cd47a015ced59467
David Bradburn <david.bradburn2@gmail.com>
2022-05-26 22:16:42 +0100

    Update 4_1.py
    Push for 26/05/2022
    added some of board checking  need to work on win check next time

--- Day 4/1/4_1.py
+++ Day 4/1/4_1.py
@@ -5,6 +5,8 @@ with open("../misc/test.txt", "r") as fd:
     boards = fd.readlines()
 
 boards = [x.strip("\n") for x in boards][1:]
+# print(boards)
+# print("____")
 boards = boards + [""]
 # print(boards)
 boards_proccesed = []
@@ -13,16 +15,65 @@ temp = []
 cnt = 0
 
 for x in boards:
-    print(x)
+    # print(x)
     if x == "":
         cnt += 1
         boards_proccesed += [temp]
-        print(temp)
+        # print(temp)
         temp = []
     else:
-        temp += [[i for i in x.split(" ") if i]]
-        # temp += [(x.split(" ")).remove("")]
+        temp += [[int(i) for i in x.split(" ") if i]]
 
-print(cnt)
-for i in range(cnt):
-    print(boards_proccesed[i])
+# print( )
+# for i in range(cnt):
+#     print(boards_proccesed[i])
+
+
+boards_proccesed_np = np.array(boards_proccesed, dtype=int)
+print(boards_proccesed_np)
+board_check_array = np.zeros(boards_proccesed_np.shape, dtype=int)
+print(board_check_array)
+
+
+
+def tick_off_value(arr, val):
+    no_of_boards, x, y = arr.shape
+    # print(no_of_boards, x, y)
+    for i in range(no_of_boards):
+        for j in range(x):
+            for k in range(y):
+                if arr[i,j,k] == val:
+                    board_check_array[i,j,k] = 1
+
+def check_row(arr):
+    no_of_boards, x, y = arr.shape
+    for i in range(no_of_boards):
+        for j in range(x): #row
+            count = 0
+            for k in range(y): #col
+                if arr[i, j, k] == 1:
+                    count += 1
+            if count == 5:
+                return True, i, j ,k
+
+    return False, -1, -1, -1
+
+def check_col(arr):
+    no_of_boards, x, y = arr.shape
+    for i in range(no_of_boards):
+        for k in range(y): #col
+            count = 0
+            for j in range(x): #row
+                if arr[i, j, k] == 1:
+                    count += 1
+            if count == 5:
+                return True, i, j ,k
+
+    return False, -1, -1, -1
+
+
+def check_win(arr):
+    win, board, row, col = 
+
+tick_off_value(boards_proccesed_np, 7)
+print(board_check_array)
