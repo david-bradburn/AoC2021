6e0f00e8562e50dccb9c7c86cf7cc32be5d63c07
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-06-03 22:06:11 +0100

    Day 8 part 1 and setup
    Day 8 part 1
    Answer: 342
    Kinda avoided having to do the problem entirely by only count string length which is nice but is basically useless for part 2

---
+++ Day 8/1/8_1.py
@@ -0,0 +1,30 @@
+
+
+file = "input.txt"
+file_path = "../misc/"
+with open(file_path + file, "r") as fd:
+    input_raw = fd.readlines()
+    input_stripped_split = []
+    for i in input_raw:
+        # print("egg")
+        temp = i.strip("\n").split("|")[1]
+        # print(temp.split(" "))
+        temp_split = temp.split(" ")
+        for i in temp_split:
+            # print(i)
+            if i == '':
+                # print("Empty")
+                pass
+            else:
+                input_stripped_split += [i]
+
+clean_input = input_stripped_split
+
+cnt = 0
+for i in clean_input:
+    match len(i):
+        case (2 | 4 | 3 | 7): ## 1, 4, 7, 8
+            cnt += 1
+
+print(cnt)
+# print(input_stripped_split)
