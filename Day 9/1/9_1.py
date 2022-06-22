87fbc9843043586337d1969c2318ddbe57ea1bd5
David Bradburn <david.bradburn2@gmail.com>
2022-06-22 21:41:49 +0100

    Day 9 part 1
    Answer: 504
    Fun little project, got it on second try.
    Kind of dreading the part 2 however

--- Day 9/1/9_1.py
+++ Day 9/1/9_1.py
@@ -2,15 +2,15 @@
 
 import numpy as np
 
-file = "test.txt"
+file = "input.txt"
 file_path = "../misc/"
 with open(file_path + file, "r") as fd:
     input_raw = fd.readlines()
     input_stripped = []
     input_stripped += [i.strip("\n") for i in input_raw]
     # print(input_stripped)
-    N = 10
-    M = 5
+    N = 100
+    M = 100
     arr = np.zeros((M, N), dtype =int)
     # print(arr)
     for i in range(M):
@@ -18,18 +18,36 @@ with open(file_path + file, "r") as fd:
             arr[i][j] = input_stripped[i][j]
 
 sea_bed = arr
-min_sea_bed = np.zeros_like(sea_bed)
-print(sea_bed)
+min_sea_bed = np.ones_like(sea_bed)
+# print(sea_bed)
+# print(min_sea_bed)
+arr_check = [-1, 1]
 
 for y in range(M):
     for x in range(N):
-        cells_to_check = []
-        match x, y:
-            case (0, 0):
-                cells_to_check = [[x + 1, y], [x, y + 1]]
-            case (0, z) if z != N: ### draw a diagram
-                cells_to_check = [[x + 1, y], [x, y + 1]]
+        for vert in arr_check:
+            try:
+                if sea_bed[y][x + vert] <= sea_bed[y][x]:
+                    min_sea_bed[y][x] = 0
+            except:
+                pass
 
+            try:
+                if sea_bed[y + vert][x] <= sea_bed[y][x]:
+                    min_sea_bed[y][x] = 0
+            except:
+                pass
+        
+                
+# print(min_sea_bed)
+risk_level = 0
+for y in range(M):
+    for x in range(N):
+        if min_sea_bed[y][x]:
+            risk_level += (sea_bed[y][x] + 1)
 
+print(risk_level)
+
+##504
 
 
