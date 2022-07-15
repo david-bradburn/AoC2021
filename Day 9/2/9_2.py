e71123934649d339447182447f0bc89b246492f2
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-07-15 16:37:28 +0100

    Update 9_2.py
    Day 9 part 2 complete
    Answer: 1558722
    
    Really enjoyed this once I decided on a method. Onto the next one!!

--- Day 9/2/9_2.py
+++ Day 9/2/9_2.py
@@ -2,15 +2,15 @@
 
 import numpy as np
 
-file = "test.txt"
+file = "input.txt"
 file_path = "../misc/"
 with open(file_path + file, "r") as fd:
     input_raw = fd.readlines()
     input_stripped = []
     input_stripped += [i.strip("\n") for i in input_raw]
     # print(input_stripped)
-    N = 10
-    M = 5
+    N = 100
+    M = 100
     arr = np.zeros((M, N), dtype =int)
     # print(arr)
     for i in range(M):
@@ -64,51 +64,49 @@ def local_min_finder(arr, current_index, current_direction_arr, route):
             # print(direction)
             match direction:
                 case "up":
-                    try:
-                        if arr[y - 1][x] < arr[y][x]:
-                            route += [(y-1,x)]
-                            return local_min_finder(arr, (y-1,x), np.delete(direction_arr_master, 2), route)
-                    except IndexError:
-                        pass
+                    if y - 1 < 0:
+                        continue
+                    if arr[y - 1][x] < arr[y][x]:
+                        route += [direction, (y-1,x)]
+                        return local_min_finder(arr, (y-1,x), np.delete(direction_arr_master, 2), route)
                 
                 case "right":
-                    try:
-                        # print(arr[y][x + 1] < arr[y][x])
-                        if arr[y][x + 1] < arr[y][x]:
-                            route += [(y,x + 1)]
-                            # print(route)
-                            return local_min_finder(arr, (y, x + 1), np.delete(direction_arr_master, 3), route)
-                    except IndexError:
-                        pass
+
+                    if x + 1 > N - 1:
+                        continue
+                    if arr[y][x + 1] < arr[y][x]:
+                        route += [direction, (y,x + 1)]
+                        # print(route)
+                        return local_min_finder(arr, (y, x + 1), np.delete(direction_arr_master, 3), route)
+
 
                 case "down":
-                    try:
-                        if arr[y + 1][x] < arr[y][x]:
-                            route += [(y+1,x)]
-                            return local_min_finder(arr, (y + 1, x), np.delete(direction_arr_master, 0), route)
-                    except IndexError:
-                        pass
+                    if y + 1 > M - 1:
+                        continue
+                    if arr[y + 1][x] < arr[y][x]:
+                        route += [direction, (y+1,x)]
+                        return local_min_finder(arr, (y + 1, x), np.delete(direction_arr_master, 0), route)
                 
                 case "left":
-                    try:
-                        if arr[y][x - 1] < arr[y][x]:
-                            route += [(y,x - 1)]
-                            return local_min_finder(arr, (y, x - 1), np.delete(direction_arr_master, 1), route)
-                    except IndexError:
-                        pass
+                    
+                    if x - 1 < 0:
+                        continue
+                    if arr[y][x - 1] < arr[y][x]:
+                        route += [direction, (y,x - 1)]
+                        return local_min_finder(arr, (y, x - 1), np.delete(direction_arr_master, 1), route)
 
 
 
 print(sea_bed)
-print(local_minima_dict)
+# print(local_minima_dict)
 for index, value in np.ndenumerate(sea_bed):
     if value == 9:
         continue
 
     route = [index]
     local_min = local_min_finder(sea_bed, index, direction_arr_master, route)
-    # print(index, value, "->", local_min)
     if local_min == None:
+        print(index, value, "->", local_min)
         print("Route", route)
 
     if (local_min in local_minima_dict):
@@ -117,9 +115,26 @@ for index, value in np.ndenumerate(sea_bed):
         print(local_min)
         print("ERROR NOT LOCAL MINIMA")
 
-print(local_minima_dict)
+# print(local_minima_dict)    
+size_list = []
+for key in local_minima_dict:
+    size_list += [local_minima_dict[key]]
 
-    # print("------------")
+print(size_list)
+max1 = max(size_list)
+size_list.remove(max1)
+print(max1)
+max2 = max(size_list)
+print(max2)
+size_list.remove(max2)
+max3 = max(size_list)
+size_list.remove(max3)
+
+print(max1, max2, max3)
+print(max1 * max2 * max3)
+
+## 1558722
+
 
 
 
