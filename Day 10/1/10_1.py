17ae04c588b754b858820f0d46cb6999c95f1daf
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-07-15 17:20:10 +0100

    Day 10 part 1
    Day 10 part 1 + setup
    Answer: 319233
    
    Very much enjoyed it, I think the fifo is lovely idea.

---
+++ Day 10/1/10_1.py
@@ -0,0 +1,57 @@
+
+
+import numpy as np
+
+file = "input.txt"
+file_path = "../misc/"
+with open(file_path + file, "r") as fd:
+    input_raw = fd.readlines()
+    input_stripped = []
+    input_stripped += [i.strip("\n") for i in input_raw]
+
+nav_syn = input_stripped
+for line in nav_syn:
+    print(line)
+
+
+syn_dict = {")" : "(", 
+            "]" : "[",
+            "}" : "{",
+            ">" : "<"}
+
+syn_dict_score = {")" : 3, 
+                  "]" : 57,
+                  "}" : 1197,
+                  ">" : 25137}
+corrupted_syn = []
+for line in nav_syn:
+    fifo = ""
+    log = ""
+
+    for bracket in line:
+        log += bracket
+
+        match bracket:
+            case "(" | "[" | "{" | "<":
+                fifo += bracket
+            
+            case default :
+                if fifo[-1] == syn_dict[bracket]:
+                    # print(fifo)
+                    fifo = fifo[:-1]
+                else:
+                    corrupted_syn += [bracket]
+                    print("fifo ", fifo)
+                    print("bracket", bracket)
+                    print("log", log)
+                    print("corrupted")
+                    print("------------------------")
+                    break
+
+total = 0
+for cor in corrupted_syn:
+    total += syn_dict_score[cor]
+
+print(total)
+
+## 319233
