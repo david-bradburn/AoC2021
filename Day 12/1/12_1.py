b4ff40b3e81435f5fd0fda9de5de208946b5641c
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-07-27 19:16:00 +0100

    Update 12_1.py
    Day 12 part 1 update:
    Added a creation of a cave network system creator, classes n stuff.
    I had no idea you could add objects to dictionaries

--- Day 12/1/12_1.py
+++ Day 12/1/12_1.py
@@ -1,3 +1,6 @@
+
+
+
 file = "test1.txt"
 
 DAY_NO = "12"
@@ -11,21 +14,59 @@ with open(file_path_base + file, "r") as fd:
 	input_stripped = []
 	input_stripped += [i.strip("\n").split("-")for i in input_raw]
 
-print(input_stripped)
+links = input_stripped
 
 
-class cave:
+class Cave:
+
 	def __init__(self, cave_name): #refactor and add cavetype
-		self.large_cave = False
-		self.small_cave = False
-		self.start_cave = False
-		self.end_cave = False 
+		 
 		self.cave_name = cave_name
 		if(self.cave_name == "start"):
-			self.start_cave = True
+			self.cave_type = "start"
 		elif(self.cave_name == "end"):
-			self.end_cave = True
+			self.cave_type = "end"
 		elif(self.cave_name.isupper()):
-			self.large_cave = True
+			self.cave_type = "large"
+		elif(self.cave_name.islower()):
+			self.cave_type = "small"
+			self.visited = False
+		else:
+			raise Exception("Cave is not valid")
+		
 
-print()
+class CaveSystem:
+
+	def create_network(self, map):
+		for cave1, cave2 in map:
+			cave1_typed = Cave(cave1)
+			cave2_typed = Cave(cave2)
+
+			if not (cave2_typed.cave_type == "start") and not (cave1_typed.cave_type == "end"):
+				if (cave1 not in self.cave_network):
+					self.cave_network[cave1] = [cave2_typed]
+				else:
+					self.cave_network[cave1] += [cave2_typed]
+
+			if not (cave1_typed.cave_type == "start") and not (cave2_typed.cave_type == "end"):
+				if cave2 not in self.cave_network:
+					self.cave_network[cave2] = [cave1_typed]
+				else:
+					self.cave_network[cave2] += [cave1_typed]
+
+	def display_cave_network(self):
+		for key in self.cave_network:
+			for cave in self.cave_network[key]:
+				print(key, cave.cave_name, cave.cave_type)
+
+	
+
+
+	def __init__(self, map):
+		self.cave_network = {}
+		self.create_network(map)
+		pass
+
+caves = CaveSystem(links)
+
+caves.display_cave_network()
