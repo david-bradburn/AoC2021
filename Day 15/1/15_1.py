3e14e9bdde74bf56ea0362b4d99dfabee8f70a01
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-07-31 15:59:26 +0100

    Day 15 part 1
    Answer: 673
    
    Nice path finding problem, implemented using an unoptimised Dijkstra's algorithm, second part is optimisation yet again. Gonna try using A*, but first might try some optimisations on this Dijkstra's :)

--- Day 15/1/15_1.py
+++ Day 15/1/15_1.py
@@ -1,4 +1,7 @@
-file = "test.txt"
+import numpy as np
+
+
+file = "input.txt"
 
 DAY_NO = "15"
 PART = "1"
@@ -6,4 +9,60 @@ PART = "1"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	input_raw = fd.readlines()
+	input_stripped = [line.strip("\n") for line in input_raw]
+	max_x = len(input_stripped[0])
+	max_y = len(input_stripped)
+	assert max_x == max_y
+	cavern = np.zeros((max_x, max_y), dtype=int)	
+	for row_index, row in enumerate(input_stripped):
+		for value_index, value in enumerate(row):
+			cavern[row_index][value_index] = value
+			pass
+	
+
+
+
+class Cavern:
+	def __init__(self, arr):
+		self.cavern = arr
+		self.risk_arr = np.ones_like(cavern, dtype=int) * 1000
+		self.open_list = [(0,0)]
+
+	def evaluate(self):
+		list_of_offsets = [(-1,  0),
+						   ( 1,  0),
+						   ( 0, -1),
+						   ( 0,  1)]
+		for x, y in self.open_list:
+			print(x, y)
+			self.risk_arr[0][0] = 0
+			for offset_x, offset_y in list_of_offsets:
+				new_y = y + offset_y
+				new_x = x + offset_x
+				if new_x >= 0 and new_y >= 0 and new_x < max_x and new_y< max_y:
+					if self.risk_arr[new_y][new_x] > self.cavern[new_y][new_x] + self.risk_arr[y][x]:
+						self.risk_arr[new_y][new_x] = self.cavern[new_y][new_x] + self.risk_arr[y][x]
+						self.open_list += [(new_x, new_y)]
+
+	def display_risk(self):
+		print(self.risk_arr)
+				
+
+
+		
+
+	def display(self):
+		for row in self.cavern:
+			temp_str = ""
+			for element in row:
+				temp_str += " {}".format(element)
+			print(temp_str)
+
+
+cavern = Cavern(cavern)
+cavern.display()
+cavern.evaluate()
+cavern.display_risk()
+
+## 673
