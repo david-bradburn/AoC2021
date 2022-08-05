53dfdfa69b201207e8a09e36d1bee9fccb24155f
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-08-05 14:21:30 +0100

    Day 18 part 1
    Update, thinking through the process of this code

--- Day 18/1/18_1.py
+++ Day 18/1/18_1.py
@@ -1,4 +1,7 @@
-file = "test.txt"
+import ast
+
+
+file = "test_explode.txt"
 
 DAY_NO = "18"
 PART = "1"
@@ -6,4 +9,19 @@ PART = "1"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	input_raw = fd.readline()
+	list_of_sn_nos = ast.literal_eval(input_raw) ## convert this is a coord esk class system.
+	
+	print(input_raw)
+	print(list_of_sn_nos)
+
+
+test_list = [[1, 2], [3, 4]]
+
+print(list.index(test_list, 2))
+
+
+# class SN_Numbers:
+
+# 	def __init__(self, ):
+
