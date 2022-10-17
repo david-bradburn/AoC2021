8d88c376f3fbada00c27d8c2e24e7d044873d890
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-10-17 10:29:10 +0100

    Day 21 Update
    Input parsing

--- Day 21/1/21_1.py
+++ Day 21/1/21_1.py
@@ -1,5 +1,5 @@
 #################################################################
-###### https://adventofcode.com/2021/day/21 #######################
+###### https://adventofcode.com/2021/day/21 #####################
 #################################################################
 
 file = "test.txt"
@@ -10,4 +10,15 @@ PART = "1"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	input_raw = fd.readlines()
+	player_1_start_raw = input_raw[0].strip('\n')[-1]
+	player_2_start_raw = input_raw[1].strip('\n')[-1]
+
+print(player_1_start_raw, player_2_start_raw)
+
+class Dirac_Board():
+
+	def __init__(self) -> None:
+		pass
+
+
