2ae532d9a5271203c5c696eec0ab4baf194fb967
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-11-18 14:12:00 +0000

    Day 22 part 1
    string parsing

--- Day 22/1/22_1.py
+++ Day 22/1/22_1.py
@@ -1,8 +1,8 @@
 #################################################################
-###### https://adventofcode.com/2021/day/22 #######################
+###### https://adventofcode.com/2021/day/22 #####################
 #################################################################
 
-file = "test.txt"
+file = "test1.txt"
 
 DAY_NO = "22"
 PART = "1"
@@ -10,4 +10,14 @@ PART = "1"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	raw_input = fd.readlines()
+	test = []
+	for row in raw_input:
+		temp = row.strip('\n').split(' ')
+		
+		match temp:
+			case [on_off, coord]:
+				print(on_off)
+				print(coord.split(','))
+
+
