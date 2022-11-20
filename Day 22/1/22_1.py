02063c6a2be2f459a59193ce27594c706911c778
David Bradburn <david.bradburn2@gmail.com>
2022-11-20 22:19:18 +0000

    Update 22_1.py

--- Day 22/1/22_1.py
+++ Day 22/1/22_1.py
@@ -9,15 +9,47 @@ PART = "1"
 
 file_path_base = "Day " + DAY_NO + "/misc/"
 
+flip_arr = []
+coords_clean = []
+
 with open(file_path_base + file, "r") as fd:
 	raw_input = fd.readlines()
-	test = []
+	
 	for row in raw_input:
 		temp = row.strip('\n').split(' ')
 		
 		match temp:
-			case [on_off, coord]:
+			case [on_off, coords_raw]:
 				print(on_off)
-				print(coord.split(','))
+				match on_off:
+					case "on":
+						flip_arr += [1]
+					case "off":
+						flip_arr += [0]
+				
+				# print(coords_raw.split(','))
+				temp_arr = []
+				for n, cd in enumerate(coords_raw.split(',')):
+
+					start1 = cd.find("=")
+					end1 = cd.find(".")
+					coord_st = int(cd[start1+1:end1])
+					coord_end = int(cd[end1+2:])
+
+					temp_arr += [[coord_st, coord_end]]
+					# print(coord_st, coord_end)
+				
+				coords_clean += [temp_arr]
+
+					
 
 
+
+			case _:
+				raise Exception("Yeah nah this aint meant to look like this")
+
+
+print(flip_arr)
+print(coords_clean)
+for i in coords_clean:
+	print(i)
